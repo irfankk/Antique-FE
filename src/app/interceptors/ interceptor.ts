43f3779da68a6eb72503089486af9c840860b44a
Irfan <irfankk1993@gmail.com>
2021-03-22 10:26:52 +0530

    remove log

--- src/app/interceptors/ interceptor.ts
+++ src/app/interceptors/ interceptor.ts
@@ -11,10 +11,9 @@ export class Interceptor implements HttpInterceptor {
     private _homeService: HomeService
   ) { }  
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    
+    request = request.clone({headers: request.headers.set('Access-Control-Allow-Origin', '*')})
     let customReq = request.clone({});
     if (this._homeService.isLoggedIn()) {
-        console.log('hhhhhhhhhhhhh')
         customReq = request.clone({
             setHeaders: {
                 Authorization: `Token ${localStorage.getItem('key')}`
